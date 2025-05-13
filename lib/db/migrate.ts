8233c7c53b6e77ec7dcd8e880cb4127bb76ffbfe
jhu7235 <jhu7235@gmail.com>
2025-05-13 14:47:37 +0200

    switch up how to loading postgres url

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -3,9 +3,12 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
-config({
-  path: '.env.local',
-});
+if (!process.env.POSTGRES_URL) {
+  config({
+    path: '.env.local',
+  });
+}
+
 
 const runMigrate = async () => {
   if (!process.env.POSTGRES_URL) {
