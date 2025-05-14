67ae428f5783590bd48e98d72c00745e15cc41fa
jhu7235 <jhu7235@gmail.com>
2025-05-14 14:38:25 +0200

    update the welcome message

--- components/overview.tsx
+++ components/overview.tsx
@@ -11,27 +11,21 @@ export const Overview = () => {
       exit={{ opacity: 0, scale: 0.98 }}
       transition={{ delay: 0.5 }}
     >
-      <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
-        <p>
-          This is Mr Pigeon&apos;s chatbot built with Next.js and the AI SDK by Vercel. It uses
-          the{' '}
-          <code className="rounded-md bg-muted px-1 py-0.5">streamText</code>{' '}
-          function in the server and the{' '}
-          <code className="rounded-md bg-muted px-1 py-0.5">useChat</code> hook
-          on the client to create a seamless chat experience.
-        </p>
-        <p>
-          You can learn more about the project by visiting{' '}
-          <Link
-            className="font-medium underline underline-offset-4"
-            href="https://github.com/jhu7235/mr-pigeon-chatbot"
-            target="_blank"
-          >
-            my github repo
-          </Link>
-          .
-        </p>
-      </div>
+    <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
+      <h2 className="text-2xl font-bold">
+        Welcome to Mr. Pigeon’s Chatbot!
+      </h2>
+
+      <p>
+        Built on <strong>Next.js  </strong>, <strong>React</strong>, <strong>Vercel AI</strong>. It wraps multiple foundational models including <strong>Deepseek</strong>, <strong>Claude</strong>, and <strong>OpenAI</strong>
+      </p>
+      <span>For more details about the underlying technologies, check out the {" "}
+        <Link href="https://github.com/jhu7235/mr-pigeon-chatbot" 
+        className="font-medium underline underline-offset-4">
+          GitHub repo
+        </Link>
+      </span>
+    </div>
     </motion.div>
   );
 };
